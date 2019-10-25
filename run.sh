900a7cb609d9006ac06c9843223e83e8b220e4d6
ymlml <luminlong@gmail.com>
2019-10-24 23:31:20 -0700

    run.sh

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 
 #python z_my_train_i3d_1.py -mode 'rgb' -save_model 'tmp/tmp' 2>&1 | tee AAA.log
 
-#python z_my_train_i3d_1.py -mode 'flow' -save_model 'tmp/tmp' 2>&1 | tee AAA.log
+python z_my_train_i3d_1.py -mode 'flow' -save_model 'tmp/tmp' 2>&1 | tee AAA.log
 
 #python z_my_train_i3d_1_attention_gaze.py -mode 'rgb' -save_model 'tmp/tmp' 2>&1 | tee AAA.log
 
-python z_my_train_i3d_1_attention_gaze.py -mode 'flow' -save_model 'tmp/tmp' 2>&1 | tee AAA.log
+#python z_my_train_i3d_1_attention_gaze.py -mode 'flow' -save_model 'tmp/tmp' 2>&1 | tee AAA.log
 
 
 echo 'Done.'
